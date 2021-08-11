e49be0015f2a53fc7d7bff6b9250602525ed0795
SMOKEROp <88759799+SMOKEROp@users.noreply.github.com>
2021-08-11 09:53:57 +0530

    Update joiner.py

--- joiner.py
+++ joiner.py
@@ -1,13 +1,16 @@
 import requests
 
-link = input('Discord Invite Link: ')
+link = input('https://discord.gg/pAWvrtx5: ')
 if len(link) > 6:
     link = link[19:]
 apilink = "https://discordapp.com/api/v6/invite/" + str(link)
 
 print (link)
 
-with open('tokens.txt','r') as handle:
+with open('ODcwNDk0ODEzOTY1OTM4Njg5.YQNlmQ.fWldrJ9nteE45M1dZ7LTJRICt5E
+ODcwNDk1NzA3MTg5MDk2NDg4.YQNmXA.tT0BufSU07imMKfXrS0FjalNXZY
+ODcwNDk2MDUzODc3NjkwMzk5.YQNnVw.tyVMG9mJqqyOzVTjPBjYUfb_CEA
+','r') as handle:
         tokens = handle.readlines()
         for x in tokens:
             token = x.rstrip()
